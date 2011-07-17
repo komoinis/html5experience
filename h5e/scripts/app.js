ce1457330ab4646dc5f74b84142e9846159f3434
komoinisw <komoinis@lavabit.com>
2011-07-17 19:04:09 +0200

    Dankesnote

--- h5e/scripts/app.js
+++ h5e/scripts/app.js
@@ -1,3 +1,40 @@
 define(["jquery","i18n!std_dict/nls/nouns"], function($, nouns) {
-	alert("messages " + nouns.root.message_pl);
+	
+	var isBrowser = true; // TODO: setze sinnvolle vorkonditionen
+	mode = isBrowser ? 'Client' : 'Server',
+
+    // Danke für http://zetafleet.com/blog/unified-codebases-with-dojo-node-and-requirejs-the-holy-grail-of-dry
+	// our main application object; anything else that
+	// requires 'my/app' in the future will receive this
+	// object (because it’s returned at the end of this
+	// function); all other defined modules work the
+	// same way: the callback is invoked once and
+	// the returned value is cached by RequireJS
+	app = {
+		onReady: function () {
+			alert("hello");
+		}
+	};
+
+/*	// loads either Client or Server class for Db and
+	// Conduit depending upon if we are on the
+	// client or server
+	require(['my/db/' + mode, 'my/conduit/' + mode,
+	'my/Baz'], function (Db, Conduit, Baz) {
+
+		app.db = new Db();
+		app.conduit = new Conduit();
+
+		// this module works exactly the same on
+		// both client and server, no extra code
+		// necessary! NICE!
+		app.baz = new Baz();
+
+		// app has loaded, fire anything that has
+		// connected to app.onReady!
+		app.onReady();
+	});*/
+	app.onReady();
+	
+	return app;
 });
